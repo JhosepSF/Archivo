48b73649dc80e1b993a92edfad1b490595eff2ed
JhosepSF <jhosepsanflo@gmail.com>
2024-06-07 08:20:33 -0500

    Arreglo del jwt otra vez

--- src/main/java/unsm/archivo/jwt/JwtAuthenticationFilter.java
+++ src/main/java/unsm/archivo/jwt/JwtAuthenticationFilter.java
@@ -18,13 +18,17 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	
 	private final JwtService jwtService;
+	public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+		super();
+		this.jwtService = jwtService;
+		this.userDetailsService = userDetailsService;
+	}
+
 	private final UserDetailsService userDetailsService;
 	
 	@SuppressWarnings("null")
