44214352d37ef2b7e27e4a39471d165ae4c0ea1b
EduGalaxy0 <eduysting@hotmail.com>
2024-06-18 13:32:49 -0500

    email dinamico add

--- src/main/java/unsm/archivo/controller/EmailController.java
+++ src/main/java/unsm/archivo/controller/EmailController.java
@@ -1,6 +1,9 @@
 package unsm.archivo.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,10 +21,17 @@ public class EmailController {
     @Autowired
     unsm.archivo.services.EmailService emailService;
 
+    @Value("${spring.mail.username}")
+    private String mailFrom;
+
   
     @PostMapping("/enviarcorreo")
     public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request ){
-    	
+    	request.setMailFrom(mailFrom);
+        request.setSubject("Cambio de Contraseña");
+        UUID uuid = UUID.randomUUID();
+        String codigo = uuid.toString();
+        request.setCodigo(codigo);
         emailService.sendEmail(request);
         return new ResponseEntity<String>("Correo con plantilla enviado", HttpStatus.OK);
     }
